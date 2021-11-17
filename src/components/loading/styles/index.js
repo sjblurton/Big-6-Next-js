5b1e79ff59c438b4f2449295484a0e9fc6a4ec75
Simon Blurton <sblurton@hotmail.co.uk>
2021-11-17 15:58:21 -0600

    updated some ui stuff

--- src/components/loading/styles/index.js
+++ src/components/loading/styles/index.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	background-color: ${(props) => props.theme.color.bgDark};
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	padding: 10%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.color.bgDark};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  flex-direction: column;
 `;
--- src/components/profile/header/menu/index.jsx
+++ src/components/profile/header/menu/index.jsx
@@ -4,35 +4,35 @@ import { useRouter } from "next/router";
 import { Wrapper, Menu } from "./styles";
 
 const Dropdown = ({ setMenuOpen }) => {
-	const router = useRouter();
-	const { logout, user } = useAuth();
-	const handleLogout = (e) => {
-		e.stopPropagation();
-		setMenuOpen(false);
-		logout();
-		router.push("/");
-	};
+  const router = useRouter();
+  const { logout, user } = useAuth();
+  const handleLogout = (e) => {
+    e.stopPropagation();
+    setMenuOpen(false);
+    logout();
+    router.push("/");
+  };
 
-	return (
-		<Wrapper onClick={() => setMenuOpen(false)} name="wrapper">
-			<Menu>
-				<ul>
-					{!user && <li onClick={() => router.push("/")}>Home</li>}
-					{user && (
-						<>
-							<li onClick={() => router.push("/profile")}>Profile</li>
-							<li onClick={() => router.push("/history")}>History</li>
-							<li onClick={() => router.push("/routines")}>Routine</li>
-						</>
-					)}
-					<li onClick={() => router.push("/timer")}>Timer</li>
-					<li onClick={() => router.push(`/directions`)}>Manual</li>
-					{user && <li onClick={handleLogout}>Logout</li>}
-					{!user && <li onClick={() => router.push("/")}>Log in</li>}
-				</ul>
-			</Menu>
-		</Wrapper>
-	);
+  return (
+    <Wrapper onClick={() => setMenuOpen(false)} name="wrapper">
+      <Menu>
+        <ul>
+          {!user && <li onClick={() => router.push("/")}>Home</li>}
+          {user && (
+            <>
+              <li onClick={() => router.push("/profile")}>Profile</li>
+              <li onClick={() => router.push("/history")}>History</li>
+              <li onClick={() => router.push("/routines")}>Routine</li>
+            </>
+          )}
+          <li onClick={() => router.push("/timer")}>Timer</li>
+          <li onClick={() => router.push(`/directions`)}>Manual</li>
+          {user && <li onClick={handleLogout}>Logout</li>}
+          {!user && <li onClick={() => router.push("/")}>Log in</li>}
+        </ul>
+      </Menu>
+    </Wrapper>
+  );
 };
 
 export default Dropdown;
