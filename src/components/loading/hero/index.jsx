200a4263f4d07b8a3c2ae344bd76d51bc9ac1b7a
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-16 12:58:56 +0100

    bug fix: logging in now only appears once.

--- src/components/loading/hero/index.jsx
+++ src/components/loading/hero/index.jsx
@@ -3,7 +3,6 @@ import { Wrapper } from "../styles";
 
 const HeroLoader = () => (
   <Wrapper>
-    <h1>Logging in</h1>
     <h1>Logging In.</h1>
     <h1>Please Wait...</h1>
   </Wrapper>
--- src/components/loading/styles/index.js
+++ src/components/loading/styles/index.js
@@ -1,19 +1,6 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-<<<<<<< HEAD
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	background-color: ${(props) => props.theme.color.bgDark};
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	padding: 0;
-=======
   position: fixed;
   top: 0;
   bottom: 0;
@@ -21,9 +8,8 @@ export const Wrapper = styled.div`
   right: 0;
   background-color: ${(props) => props.theme.color.bgDark};
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 0;
-  flex-direction: column;
->>>>>>> 0857ea77f724915dbd4fa4d09c0d7956c23ed1d2
 `;
